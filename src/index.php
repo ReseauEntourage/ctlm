9c383ebe114bc502c7472352d117d3dae54d6dd8
Grégoire Clermont <gregoire@entourage.social>
2018-05-02 19:47:23 +0200

    Fix a warning (uninitialized constant)

--- src/index.php
+++ src/index.php
@@ -3,22 +3,22 @@
  * The main template file
  */
 ?>
-<?php 
+<?php
 	get_template_part( 'head', get_post_format() );
 	get_template_part( 'menu', get_post_format() );
-	if ($_GET[page]=='partenaires'){
+	if ($_GET['page']=='partenaires'){
 	    get_template_part( 'partenaires', get_post_format() );
-	} else if ($_GET[page]=='medias'){
+	} else if ($_GET['page']=='medias'){
 	    get_template_part( 'medias', get_post_format() );
-	} else if ($_GET[page]=='actualites'){
+	} else if ($_GET['page']=='actualites'){
 	    get_template_part( 'actualites', get_post_format() );
-	} else if ($_GET[page]=='projections'){
+	} else if ($_GET['page']=='projections'){
 	    get_template_part( 'projections', get_post_format() );
-	} else if ($_GET[page]=='filmequipe'){
+	} else if ($_GET['page']=='filmequipe'){
 	    get_template_part( 'filmequipe', get_post_format() );
-	} else if ($_GET[page]=='webserie'){
+	} else if ($_GET['page']=='webserie'){
 	    get_template_part( 'webserie', get_post_format() );
-	} else if ($_GET[page]=='contact'){
+	} else if ($_GET['page']=='contact'){
 	    get_template_part( 'contact', get_post_format() );
 	} else {
         get_template_part( 'accueil', get_post_format() );
